0337134ff531256dd5d8f0ba1d3479492a038935
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-10-27 23:50:14 +0300

    Squaring

---
+++ 5.5.1/5.5.1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+#include <vector>
+
+template <class T>
+T sqr(T a) {
+    return a * a;
+}
+
+template <typename T>
+ void sqr( std::vector<T>& vi) {
+     for (int i = 0; i < vi.size(); i++) {
+         vi[i] = vi[i] * vi[i];
+     }
+}
+
+int main()
+{
+    int a = 0;
+    std::vector<int> v{ -1,4,8 };
+
+    std::cout << "[IN]: ";
+    std::cin >> a;
+    std::cout << "[OUT]: "<<sqr(a)<<"\n";
+
+    std::cout << "[IN]: ";
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " ";
+    }std::cout << '\n';
+   
+    sqr(v);
+    std::cout << "[OUT]: ";
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " ";
+    }
+ 
+    return 0;
+}
